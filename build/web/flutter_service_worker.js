1474e435f33b9f337833c32df462b7482a2e9943
kingsman <kingsman9893@gmail.com>
2024-06-10 15:41:27 +0530

    update 10-06 commit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -41,7 +41,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fbc1c57481598ad5e4c0fd68df98bdec
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "309d0440ca67bbe0236e8ae9bf347b19",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "0472265c6313b5ccfc2aa785eabf9317",
+"flutter_bootstrap.js": "2b090adb0708da1772fba36de1919acc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
